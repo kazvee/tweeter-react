3a35a7523087969c00005c24d95bb01264aeefbc
karvok <kayvee123@gmail.com>
2023-08-28 12:14:24 -0600

    Prevent default behaviors on the TweetForm

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -5,9 +5,9 @@ const TweetForm = () => {
 
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form method="post" action="/tweets" className="newtweet__form" onSubmit={(event) => event.preventDefault()}>
         <textarea className="form__textarea" name="text" placeholder={tagline}></textarea>
-        <input type="submit" value="Tweet" className="form__input" />
+        <input type="submit" value="Tweet" className="form__input" onClick={(event) => event.preventDefault()} />
         <span className="form__counter">140</span>
       </form>
     </section>
